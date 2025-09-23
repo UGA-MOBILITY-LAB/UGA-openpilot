29a6d1b35a2e48d2bc45356f060077cdb92b34c3
Vincent Wright <1850511+vincentw56@users.noreply.github.com>
2025-09-22 21:17:00 -0700

    RealFast v0.10.3 release
    https://github.com/realfast/openpilot

--- opendbc_repo/opendbc/car/chrysler/carcontroller.py
+++ opendbc_repo/opendbc/car/chrysler/carcontroller.py
@@ -1,9 +1,12 @@
 from opendbc.can import CANPacker
-from opendbc.car import Bus, DT_CTRL
+from opendbc.car import Bus, DT_CTRL, structs
 from opendbc.car.lateral import apply_meas_steer_torque_limits
 from opendbc.car.chrysler import chryslercan
-from opendbc.car.chrysler.values import RAM_CARS, CarControllerParams, ChryslerFlags
+from opendbc.car.chrysler.values import RAM_CARS, RAM_DT, CarControllerParams, ChryslerFlags
 from opendbc.car.interfaces import CarControllerBase
+
+# RealFast variables
+GearShifter = structs.CarState.GearShifter
 
 
 class CarController(CarControllerBase):
@@ -31,12 +34,12 @@ class CarController(CarControllerBase):
       # ACC cancellation
       if CC.cruiseControl.cancel:
         self.last_button_frame = self.frame
-        can_sends.append(chryslercan.create_cruise_buttons(self.packer, CS.button_counter + 1, das_bus, cancel=True))
+        can_sends.append(chryslercan.create_cruise_buttons(self.packer, CS.button_counter + 1, das_bus, CS.button_message, cancel=True))
 
       # ACC resume from standstill
       elif CC.cruiseControl.resume:
         self.last_button_frame = self.frame
-        can_sends.append(chryslercan.create_cruise_buttons(self.packer, CS.button_counter + 1, das_bus, resume=True))
+        can_sends.append(chryslercan.create_cruise_buttons(self.packer, CS.button_counter + 1, das_bus, CS.button_message, resume=True))
 
     # HUD alerts
     if self.frame % 25 == 0:
@@ -50,7 +53,12 @@ class CarController(CarControllerBase):
 
       # TODO: can we make this more sane? why is it different for all the cars?
       lkas_control_bit = self.lkas_control_bit_prev
-      if CS.out.vEgo > self.CP.minSteerSpeed:
+      if self.CP.carFingerprint in RAM_DT:
+        if CS.out.vEgo >= self.CP.minEnableSpeed:
+          lkas_control_bit = True
+        if CS.out.gearShifter != GearShifter.drive:
+          lkas_control_bit = False
+      elif CS.out.vEgo > self.CP.minSteerSpeed:
         lkas_control_bit = True
       elif self.CP.flags & ChryslerFlags.HIGHER_MIN_STEERING_SPEED:
         if CS.out.vEgo < (self.CP.minSteerSpeed - 3.0):
--- opendbc_repo/opendbc/car/chrysler/interface.py
+++ opendbc_repo/opendbc/car/chrysler/interface.py
@@ -62,6 +62,12 @@ class CarInterface(CarInterfaceBase):
       if any(fw.ecu == 'eps' and b"68" < fw.fwVersion[:4] <= b"6831" for fw in car_fw):
         ret.minSteerSpeed = 0.
 
+      # RealFast variables
+      else:
+        ret.minSteerSpeed = 0.5
+
+      ret.minEnableSpeed = 14.5
+
     elif candidate == CAR.RAM_HD_5TH_GEN:
       ret.steerActuatorDelay = 0.2
 
