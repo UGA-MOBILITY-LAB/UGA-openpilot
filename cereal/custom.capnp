HeyLowOwO/WhoFP

FrogPilot structs

cereal/custom.capnp
@@ -1,6 +1,8 @@
 using Cxx = import "./include/c++.capnp";
 $Cxx.namespace("cereal");
 
+using Car = import "car.capnp";
+
 @0xb526ba661d550a59;
 
 # custom.capnp: a home for empty structs reserved for custom forks
@@ -10,31 +12,222 @@ $Cxx.namespace("cereal");
 # DO rename the structs
 # DON'T change the identifier (e.g. @0x81c2f05a394cf4af)
 
-struct CustomReserved0 @0x81c2f05a394cf4af {
+struct FrogPilotCarControl @0x81c2f05a394cf4af {
+  hudControl @0 :HUDControl;
+
+  struct HUDControl {
+    audibleAlert @0 :AudibleAlert;
+
+    enum AudibleAlert {
+      none @0;
+
+      engage @1;
+      disengage @2;
+      refuse @3;
+
+      warningSoft @4;
+      warningImmediate @5;
+
+      prompt @6;
+      promptRepeat @7;
+      promptDistracted @8;
+
+      # Random Events
+      angry @9;
+      continued @10;
+      dejaVu @11;
+      doc @12;
+      fart @13;
+      firefox @14;
+      goat @15;
+      hal9000 @16;
+      mail @17;
+      nessie @18;
+      noice @19;
+      startup @20;
+      thisIsFine @21;
+      uwu @22;
+    }
+  }
 }
 
-struct CustomReserved1 @0xaedffd8f31e7b55d {
+struct FrogPilotCarParams @0xaedffd8f31e7b55d {
+  alternativeExperience @0 :Int16;
+  canUsePedal @1 :Bool;
+  canUseSDSU @2 :Bool;
+  flags @3 :UInt32;
+  isHDA2 @4 :Bool;
+  openpilotLongitudinalControlDisabled @5 :Bool;
+  safetyConfigs @6 :List(SafetyConfig);
+
+  struct SafetyConfig {
+    safetyParam @0 :UInt16;
+  }
 }
 
-struct CustomReserved2 @0xf35cc4560bbf6ec2 {
+struct FrogPilotCarState @0xf35cc4560bbf6ec2 {
+  accelPressed @0 :Bool;
+  alwaysOnLateralEnabled @1 :Bool;
+  brakeLights @2 :Bool;
+  dashboardSpeedLimit @3 :Float32;
+  decelPressed @4 :Bool;
+  distancePressed @5 :Bool;
+  distanceLongPressed @6 :Bool;
+  distanceVeryLongPressed @7 :Bool;
+  ecoGear @8 :Bool;
+  forceCoast @9 :Bool;
+  pauseLateral @10 :Bool;
+  pauseLongitudinal @11 :Bool;
+  sportGear @12 :Bool;
+  trafficModeEnabled @13 :Bool;
 }
 
-struct CustomReserved3 @0xda96579883444c35 {
+struct FrogPilotDeviceState @0xda96579883444c35 {
+  freeSpace @0 :Int16;
+  usedSpace @1 :Int16;
 }
 
-struct CustomReserved4 @0x80ae746ee2596b11 {
+struct FrogPilotModelDataV2 @0x80ae746ee2596b11 {
+  turnDirection @0 :TurnDirection;
+
+  enum TurnDirection {
+    none @0;
+    turnLeft @1;
+    turnRight @2;
+  }
 }
 
-struct CustomReserved5 @0xa5cd762cd951a455 {
+struct FrogPilotOnroadEvent @0xa5cd762cd951a455 {
+  name @0 :EventName;
+
+  enable @1 :Bool;
+  noEntry @2 :Bool;
+  warning @3 :Bool;
+  userDisable @4 :Bool;
+  softDisable @5 :Bool;
+  immediateDisable @6 :Bool;
+  preEnable @7 :Bool;
+  permanent @8 :Bool;
+  overrideLateral @10 :Bool;
+  overrideLongitudinal @9 :Bool;
+
+  enum EventName {
+    blockUser @0;
+    customStartupAlert @1;
+    forcingStop @2;
+    goatSteerSaturated @3;
+    greenLight @4;
+    holidayActive @5;
+    laneChangeBlockedLoud @6;
+    leadDeparting @7;
+    noLaneAvailable @8;
+    nnffLoaded @9;
+    openpilotCrashed @10;
+    pedalInterceptorNoBrake @11;
+    speedLimitChanged @12;
+    trafficModeActive @13;
+    trafficModeInactive @14;
+    turningLeft @15;
+    turningRight @16;
+
+    # Random Events
+    accel30 @17;
+    accel35 @18;
+    accel40 @19;
+    dejaVuCurve @20;
+    firefoxSteerSaturated @21;
+    hal9000 @22;
+    openpilotCrashedRandomEvent @23;
+    thisIsFineSteerSaturated @24;
+    toBeContinued @25;
+    vCruise69 @26;
+    yourFrogTriedToKillMe @27;
+    youveGotMail @28;
+  }
 }
 
-struct CustomReserved6 @0xf98d843bfd7004a3 {
+struct FrogPilotPlan @0xf98d843bfd7004a3 {
+  accelerationJerk @0 :Float32;
+  cscControllingSpeed @1 :Bool;
+  cscSpeed @2 :Float32;
+  cscTraining @3 :Bool;
+  dangerFactor @4 :Float32;
+  dangerJerk @5 :Float32;
+  desiredFollowDistance @6 :Int64;
+  experimentalMode @7 :Bool;
+  forcingStop @8 :Bool;
+  forcingStopLength @9 :Float32;
+  frogpilotEvents @10 :List(FrogPilotOnroadEvent);
+  increasedStoppedDistance @11 :Float32;
+  lateralCheck @12 :Bool;
+  laneWidthLeft @13 :Float32;
+  laneWidthRight @14 :Float32;
+  maxAcceleration @15 :Float32;
+  minAcceleration @16 :Float32;
+  redLight @17 :Bool;
+  roadCurvature @18 :Float32;
+  slcMapSpeedLimit @19 :Float32;
+  slcMapboxSpeedLimit @20 :Float32;
+  slcNextSpeedLimit @21 :Float32;
+  slcOverriddenSpeed @22 :Float32;
+  slcSpeedLimit @23 :Float32;
+  slcSpeedLimitOffset @24 :Float32;
+  slcSpeedLimitSource @25 :Text;
+  speedJerk @26 :Float32;
+  speedLimitChanged @27 :Bool;
+  tFollow @28 :Float32;
+  themeUpdated @29 :Bool;
+  togglesUpdated @30 :Bool;
+  unconfirmedSlcSpeedLimit @31 :Float32;
+  vCruise @32 :Float32;
+  weatherDaytime @33 :Bool;
+  weatherId @34 :Int16;
 }
 
-struct CustomReserved7 @0xb86e6369214c01c8 {
+struct FrogPilotRadarState @0xb86e6369214c01c8 {
+  leadLeft @0 :LeadData;
+  leadRight @1 :LeadData;
+
+  struct LeadData {
+    dRel @0 :Float32;
+    yRel @1 :Float32;
+    vRel @2 :Float32;
+    aRel @3 :Float32;
+    vLead @4 :Float32;
+    dPath @5 :Float32;
+    vLat @6 :Float32;
+    vLeadK @7 :Float32;
+    aLeadK @8 :Float32;
+    fcw @9 :Bool;
+    status @10 :Bool;
+    aLeadTau @11 :Float32;
+    modelProb @12 :Float32;
+    radar @13 :Bool;
+    radarTrackId @14 :Int32 = -1;
+  }
 }
 
-struct CustomReserved8 @0xf416ec09499d9d19 {
+struct FrogPilotSelfdriveState @0xf416ec09499d9d19 {
+  alertText1 @0 :Text;
+  alertText2 @1 :Text;
+  alertStatus @2 :AlertStatus;
+  alertSize @3 :AlertSize;
+  alertType @4 :Text;
+  alertSound @5 :Car.CarControl.HUDControl.AudibleAlert;
+
+  enum AlertStatus {
+    normal @0;
+    userPrompt @1;
+    critical @2;
+    frogpilot @3;
+  }
+
+  enum AlertSize {
+    none @0;
+    small @1;
+    mid @2;
+    full @3;
+  }
 }
 
 struct CustomReserved9 @0xa1680744031fdb2d {

frogpilot/controls/lib/frogpilot_events.py
@@ -1,0 +1,24 @@
+#!/usr/bin/env python3
+from openpilot.selfdrive.selfdrived.events import ET, EVENT_NAME, FROGPILOT_EVENT_NAME, EventName, FrogPilotEventName, Events
+
+class FrogPilotEvents:
+  def __init__(self, FrogPilotPlanner):
+    self.frogpilot_planner = FrogPilotPlanner
+
+    self.events = Events(frogpilot=True)
+
+    self.startup_seen = False
+
+    self.played_events = set()
+
+  def update(self, v_cruise, sm):
+    current_alert = sm["selfdriveState"].alertType
+    current_frogpilot_alert = sm["selfdriveState"].alertType
+
+    alerts_empty = all(sm[state].alertText1 == "" and sm[state].alertText2 == "" for state in ["selfdriveState", "frogpilotSelfdriveState"])
+
+    self.events.clear()
+
+    self.startup_seen |= sm["frogpilotSelfdriveState"].alertText1 == frogpilot_toggles.startup_alert_top and sm["frogpilotSelfdriveState"].alertText2 == frogpilot_toggles.startup_alert_bottom
+
+    self.played_events.update(FROGPILOT_EVENT_NAME[event] for event in self.events.names)

selfdrive/ui/soundd.py
@@ -4,7 +4,7 @@ import time
 import wave
 
 
-from cereal import car, messaging
+from cereal import car, custom, messaging
 from openpilot.common.basedir import BASEDIR
 from openpilot.common.filter_simple import FirstOrderFilter
 from openpilot.common.params import Params
@@ -32,6 +32,7 @@ if HARDWARE.get_device_type() == "tizi":
 AudibleAlert = car.CarControl.HUDControl.AudibleAlert
 
 # FrogPilot variables
+FrogPilotAudibleAlert = custom.FrogPilotCarControl.HUDControl.AudibleAlert
 
 
 sound_list: dict[int, tuple[str, int | None, float]] = {
@@ -134,6 +135,9 @@ class Soundd:
       new_alert = sm['selfdriveState'].alertSound.raw
 
       # FrogPilot variables
+      new_frogpilot_alert = sm['frogpilotSelfdriveState'].alertSound.raw
+      if new_alert == AudibleAlert.none and new_frogpilot_alert != FrogPilotAudibleAlert.none:
+        new_alert = new_frogpilot_alert
 
       self.update_alert(new_alert)
     elif check_selfdrive_timeout_alert(sm):
@@ -161,6 +165,7 @@ class Soundd:
     sm = messaging.SubMaster(['selfdriveState', 'soundPressure'])
 
     # FrogPilot variables
+    sm = sm.extend(['frogpilotSelfdriveState', 'frogpilotPlan'])
 
     with self.get_stream(sd) as stream:
       rk = Ratekeeper(20)

system/manager/manager.py
@@ -142,6 +142,8 @@ def manager_thread() -> None:
   ignition_prev = False
 
   # FrogPilot variables
+  sm = sm.extend(['frogpilotPlan'])
+
   params_memory = Params(memory=True, return_defaults=True)
 
   while True:

UGA-MOBILITY-LAB/UGA-openpilot

cereal/custom.capnp
@@ -1,6 +1,8 @@
 using Cxx = import "./include/c++.capnp";
 $Cxx.namespace("cereal");
 
+using Car = import "car.capnp";
+
 @0xb526ba661d550a59;
 
 # custom.capnp: a home for empty structs reserved for custom forks
@@ -10,34 +12,222 @@ $Cxx.namespace("cereal");
 # DO rename the structs
 # DON'T change the identifier (e.g. @0x81c2f05a394cf4af)
 
-struct CustomReserved0 @0x81c2f05a394cf4af {
+struct FrogPilotCarControl @0x81c2f05a394cf4af {
+  hudControl @0 :HUDControl;
+
+  struct HUDControl {
+    audibleAlert @0 :AudibleAlert;
+
+    enum AudibleAlert {
+      none @0;
+
+      engage @1;
+      disengage @2;
+      refuse @3;
+
+      warningSoft @4;
+      warningImmediate @5;
+
+      prompt @6;
+      promptRepeat @7;
+      promptDistracted @8;
+
+      # Random Events
+      angry @9;
+      continued @10;
+      dejaVu @11;
+      doc @12;
+      fart @13;
+      firefox @14;
+      goat @15;
+      hal9000 @16;
+      mail @17;
+      nessie @18;
+      noice @19;
+      startup @20;
+      thisIsFine @21;
+      uwu @22;
+    }
+  }
 }
 
-struct CustomReserved1 @0xaedffd8f31e7b55d {
+struct FrogPilotCarParams @0xaedffd8f31e7b55d {
+  alternativeExperience @0 :Int16;
+  flags @1 :UInt32;
+  openpilotLongitudinalControlDisabled @2 :Bool;
+  safetyConfigs @3 :List(SafetyConfig);
+
+  struct SafetyConfig {
+    safetyParam @0 :UInt16;
+  }
 }
 
-struct CustomReserved2 @0xf35cc4560bbf6ec2 {
+struct FrogPilotCarState @0xf35cc4560bbf6ec2 {
+  accelPressed @0 :Bool;
+  alwaysOnLateralEnabled @1 :Bool;
+  brakeLights @2 :Bool;
+  dashboardSpeedLimit @3 :Float32;
+  decelPressed @4 :Bool;
+  distancePressed @5 :Bool;
+  distanceLongPressed @6 :Bool;
+  distanceVeryLongPressed @7 :Bool;
+  ecoGear @8 :Bool;
+  forceCoast @9 :Bool;
+  pauseLateral @10 :Bool;
+  pauseLongitudinal @11 :Bool;
+  sportGear @12 :Bool;
+  trafficModeEnabled @13 :Bool;
 }
 
-struct CustomReserved3 @0xda96579883444c35 {
+struct FrogPilotDeviceState @0xda96579883444c35 {
+  freeSpace @0 :Int16;
+  usedSpace @1 :Int16;
 }
 
-struct CustomReserved4 @0x80ae746ee2596b11 {
+struct FrogPilotModelDataV2 @0x80ae746ee2596b11 {
+  turnDirection @0 :TurnDirection;
+
+  enum TurnDirection {
+    none @0;
+    turnLeft @1;
+    turnRight @2;
+  }
 }
 
-struct CustomReserved5 @0xa5cd762cd951a455 {
+struct FrogPilotOnroadEvent @0xa5cd762cd951a455 {
+  name @0 :EventName;
+
+  enable @1 :Bool;
+  noEntry @2 :Bool;
+  warning @3 :Bool;
+  userDisable @4 :Bool;
+  softDisable @5 :Bool;
+  immediateDisable @6 :Bool;
+  preEnable @7 :Bool;
+  permanent @8 :Bool;
+  overrideLateral @10 :Bool;
+  overrideLongitudinal @9 :Bool;
+
+  enum EventName {
+    blockUser @0;
+    customStartupAlert @1;
+    forcingStop @2;
+    goatSteerSaturated @3;
+    greenLight @4;
+    holidayActive @5;
+    laneChangeBlockedLoud @6;
+    leadDeparting @7;
+    noLaneAvailable @8;
+    nnffLoaded @9;
+    openpilotCrashed @10;
+    pedalInterceptorNoBrake @11;
+    speedLimitChanged @12;
+    trafficModeActive @13;
+    trafficModeInactive @14;
+    turningLeft @15;
+    turningRight @16;
+
+    # Random Events
+    accel30 @17;
+    accel35 @18;
+    accel40 @19;
+    dejaVuCurve @20;
+    firefoxSteerSaturated @21;
+    hal9000 @22;
+    openpilotCrashedRandomEvent @23;
+    thisIsFineSteerSaturated @24;
+    toBeContinued @25;
+    vCruise69 @26;
+    yourFrogTriedToKillMe @27;
+    youveGotMail @28;
+  }
 }
 
-struct CustomReserved6 @0xf98d843bfd7004a3 {
+struct FrogPilotPlan @0xf98d843bfd7004a3 {
+  accelerationJerk @0 :Float32;
+  cscControllingSpeed @1 :Bool;
+  cscSpeed @2 :Float32;
+  cscTraining @3 :Bool;
+  dangerFactor @4 :Float32;
+  dangerJerk @5 :Float32;
+  desiredFollowDistance @6 :Int64;
+  experimentalMode @7 :Bool;
+  forcingStop @8 :Bool;
+  forcingStopLength @9 :Float32;
+  frogpilotEvents @10 :List(FrogPilotOnroadEvent);
+  increasedStoppedDistance @11 :Float32;
+  laneWidthLeft @12 :Float32;
+  laneWidthRight @13 :Float32;
+  lateralCheck @14 :Bool;
+  maxAcceleration @15 :Float32;
+  minAcceleration @16 :Float32;
+  redLight @17 :Bool;
+  roadCurvature @18 :Float32;
+  slcMapSpeedLimit @19 :Float32;
+  slcMapboxSpeedLimit @20 :Float32;
+  slcNextSpeedLimit @21 :Float32;
+  slcOverriddenSpeed @22 :Float32;
+  slcSpeedLimit @23 :Float32;
+  slcSpeedLimitOffset @24 :Float32;
+  slcSpeedLimitSource @25 :Text;
+  speedJerk @26 :Float32;
+  speedLimitChanged @27 :Bool;
+  tFollow @28 :Float32;
+  themeUpdated @29 :Bool;
+  unconfirmedSlcSpeedLimit @30 :Float32;
+  vCruise @31 :Float32;
+  weatherDaytime @32 :Bool;
+  weatherId @33 :Int16;
 }
 
-struct CustomReserved7 @0xb86e6369214c01c8 {
+struct FrogPilotRadarState @0xb86e6369214c01c8 {
+  leadLeft @0 :LeadData;
+  leadRight @1 :LeadData;
+
+  struct LeadData {
+    dRel @0 :Float32;
+    yRel @1 :Float32;
+    vRel @2 :Float32;
+    aRel @3 :Float32;
+    vLead @4 :Float32;
+    dPath @5 :Float32;
+    vLat @6 :Float32;
+    vLeadK @7 :Float32;
+    aLeadK @8 :Float32;
+    fcw @9 :Bool;
+    status @10 :Bool;
+    aLeadTau @11 :Float32;
+    modelProb @12 :Float32;
+    radar @13 :Bool;
+    radarTrackId @14 :Int32 = -1;
+  }
 }
 
-struct CustomReserved8 @0xf416ec09499d9d19 {
+struct FrogPilotSelfdriveState @0xf416ec09499d9d19 {
+  alertText1 @0 :Text;
+  alertText2 @1 :Text;
+  alertStatus @2 :AlertStatus;
+  alertSize @3 :AlertSize;
+  alertType @4 :Text;
+  alertSound @5 :FrogPilotCarControl.HUDControl.AudibleAlert;
+
+  enum AlertStatus {
+    normal @0;
+    userPrompt @1;
+    critical @2;
+    frogpilot @3;
+  }
+
+  enum AlertSize {
+    none @0;
+    small @1;
+    mid @2;
+    full @3;
+  }
 }
 
-struct CustomReserved9 @0xa1680744031fdb2d {
+struct FrogPilotUI @0xa1680744031fdb2d {
+  frogpilotToggles @0 :Text;
 }
 
 struct CustomReserved10 @0xcb9fd56c7057593a {

frogpilot/controls/lib/frogpilot_events.py
@@ -1,0 +1,26 @@
+#!/usr/bin/env python3
+from openpilot.selfdrive.selfdrived.events import ET, EVENT_NAME, FROGPILOT_EVENT_NAME, EventName, FrogPilotEventName, Events
+
+from openpilot.frogpilot.common import frogpilot_variables
+
+class FrogPilotEvents:
+  def __init__(self, FrogPilotPlanner):
+    self.frogpilot_planner = FrogPilotPlanner
+
+    self.events = Events(frogpilot=True)
+
+    self.startup_seen = False
+
+    self.played_events = set()
+
+  def update(self, long_control_active, sm):
+    current_alert = sm["selfdriveState"].alertType
+    current_frogpilot_alert = sm["frogpilotSelfdriveState"].alertType
+
+    alerts_empty = all(sm[state].alertText1 == "" and sm[state].alertText2 == "" for state in ["selfdriveState", "frogpilotSelfdriveState"])
+
+    self.events.clear()
+
+    self.startup_seen |= sm["frogpilotSelfdriveState"].alertText1 == frogpilot_toggles.startup_alert_top and sm["frogpilotSelfdriveState"].alertText2 == frogpilot_toggles.startup_alert_bottom
+
+    self.played_events.update(FROGPILOT_EVENT_NAME[event] for event in self.events.names)

selfdrive/ui/soundd.py
@@ -4,7 +4,7 @@ import time
 import wave
 
 
-from cereal import car, messaging
+from cereal import car, custom, messaging
 from openpilot.common.basedir import BASEDIR
 from openpilot.common.filter_simple import FirstOrderFilter
 from openpilot.common.realtime import Ratekeeper
@@ -31,6 +31,7 @@ if HARDWARE.get_device_type() == "tizi":
 AudibleAlert = car.CarControl.HUDControl.AudibleAlert
 
 # FrogPilot variables
+FrogPilotAudibleAlert = custom.FrogPilotCarControl.HUDControl.AudibleAlert
 
 
 sound_list: dict[int, tuple[str, int | None, float]] = {
@@ -131,6 +132,8 @@ class Soundd:
       new_alert = sm['selfdriveState'].alertSound.raw
 
       # FrogPilot variables
+      if new_alert == AudibleAlert.none:
+        new_alert = sm['frogpilotSelfdriveState'].alertSound.raw
 
       self.update_alert(new_alert)
     elif check_selfdrive_timeout_alert(sm):
@@ -158,6 +161,7 @@ class Soundd:
     sm = messaging.SubMaster(['selfdriveState', 'soundPressure'])
 
     # FrogPilot variables
+    sm = sm.extend(['frogpilotSelfdriveState', 'frogpilotUI'])
 
     with self.get_stream(sd) as stream:
       rk = Ratekeeper(20)

system/manager/manager.py
@@ -137,6 +137,8 @@ def manager_thread() -> None:
   ignition_prev = False
 
   # FrogPilot variables
+  sm = sm.extend(['frogpilotUI'])
+
   params_memory = Params(memory=True)
 
   while True: